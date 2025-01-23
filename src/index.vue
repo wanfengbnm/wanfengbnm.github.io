03daee7bac612e4b4d23cddd7b050ff36d5bcbb2
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2025-01-23 08:29:25 +0800

    index

---
+++ src/index.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+</script>
+
+<template>
+    <div class="divider"></div>
+    <br>
+    <div style="display: flex; justify-content: center;">
+        <img src="/test5.jpg" alt="picture" style="border-radius: 25px; height:300px; width:500px" />
+        <img src="/test2.jpg" alt="picture" style="border-radius: 25px; height:300px; width:500px" />
+
+    </div>
+    <div style="display: flex; justify-content: center;">
+        <img src="/test3.jpg" alt="picture" style="border-radius: 25px; height:300px; width:500px" />
+        <img src="/test4.jpg" alt="picture" style="border-radius: 25px; height:300px; width:500px" />
+
+    </div>
+</template>
+
+<style lang="css" scoped>
+.divider {
+    margin: 24px auto 18px;
+    width: 300px;
+    height: 1px;
+    background-color: var(--vp-c-divider);
+}
+</style>
